4a5c3560f58d7d082f600818b03d83efdbaf958c
Gabriel F <gsf4@cin.ufpe.br>
2018-10-06 16:26:40 -0300

    criação de paginas estaticas

--- getXP/config/routes.rb
+++ getXP/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'home' => 'static_pages/home'
+  get 'help' => 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

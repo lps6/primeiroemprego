27116d2e20662729bdccf2a6f3615966fd081e97
gsf4 <gsf4@cin.ufpe.br>
2018-10-07 21:48:51 +0000

    Paginas estaticas inicio 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  get 'help' => 'static_pages/help'
+  get 'help' => 'StaticPages#help'
   root 'post#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

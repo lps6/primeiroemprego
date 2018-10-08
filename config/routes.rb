6d4e9b554fdd229ad2614de15ccb71e730bd2f21
lucas <lps6@cin.ufpe.br>
2018-10-08 18:13:47 -0300

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  get "help" => 'Static_Pages#help'
+  #get "help" => 'Static_Pages#help'
   get 'static_pages/help'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

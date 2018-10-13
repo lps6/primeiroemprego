157396b7182f87ea2670a79f5934ea9f5b82efab
lps6 <lps6@cin.ufpe.br>
2018-10-13 11:37:05 -0300

    att routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   #get "help" => 'Static_Pages#help'
   get 'static_pages/help'
-  root 'static_pages#home'
+  root :to => 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

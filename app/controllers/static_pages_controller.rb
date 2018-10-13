3280a91f5a1f7f834883a08b39eddba5741e23e9
lps6 <lps6@cin.ufpe.br>
2018-10-13 11:17:24 -0300

    removed text from view and put in controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    title = 'GetXp'
+    jumbotron = 'Nossa missão é conectar profissionais com pouca ou nenhuma experiência a 
+    empresas dispostas a dar primeiras oportunidades.'
   end
 
   def help

36d10d4925d59652be0f03805c4d06ccc261826d
ewerton <effs@cin.ufpe.br>
2018-10-20 08:40:22 -0300

    Estrutura inicial de cadastro. Novas counas no migrate + controller

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,7 @@
+class ProfileController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profile/new'
+  get 'profile/edit'
   devise_for :users
   resources :dashboard
   get 'static_pages/home'

34ef08362f426fef3e8d58d9969f583acc0296da
lps6 <lps6@cin.ufpe.br>
2018-10-27 16:20:13 -0300

    tagline for companies modified

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,6 +14,8 @@ class StaticPagesController < ApplicationController
     @titleforcompanies = 'Quer contratar pelo GetXp?'
     
     @jumbotronforcompanies = 'Nossa missão é conectar profissionais e empresas. Crie suas missões ou veja os resultados das missões já existentes e traga para seu time um profissional alinhado com seus valores e necessidades!'
+  
+    @taglineforcompanies = 'Pront@s para sua primeira missão?'
   end
 
   def help

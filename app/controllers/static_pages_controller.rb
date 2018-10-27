1b5d867dda5533033fca5e79f848574d3b179606
lps6 <lps6@cin.ufpe.br>
2018-10-27 16:12:27 -0300

    jumbotron para empresas adicionado

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,6 +10,10 @@ class StaticPagesController < ApplicationController
     @jumbotron = 'Nossa missão é conectar profissionais e empresas. Realize missões e conquiste seu espaço no mercado!'
     
     @tagline = 'Pront@ para sua primeira missão?'
+
+    @title = 'Quer contratar pelo GetXp?'
+    
+    @jumbotron = 'Nossa missão é conectar profissionais e empresas. Crie suas missões ou veja os resultados das missões já existentes e traga para seu time um profissional alinhado com seus valores e necessidades!'
   end
 
   def help

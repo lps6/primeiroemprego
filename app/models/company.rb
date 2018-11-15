8764d36ee86d370f995bb28ea3cc69476b88b795
ewerton <effs@cin.ufpe.br>
2018-11-15 10:49:31 -0300

    Adicionado cards das quests e conversão de haml => erb das views

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,4 @@
 class Company < ApplicationRecord
-  has_many :quests
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,5 +1,4 @@
 class Quest < ApplicationRecord
-    belongs_to :company
     has_many :question_grids, inverse_of: :quest
     accepts_nested_attributes_for :question_grids, reject_if: :all_blank, allow_destroy: true
 end

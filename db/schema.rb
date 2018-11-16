04b6fa86dddc6b36816f4ae3f204a952fb335ecb
ewerton <effs@cin.ufpe.br>
2018-11-16 00:37:16 -0300

    Ajustes visuais

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2018_11_15_191617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "answer_grids", force: :cascade do |t|
+    t.integer "order"
+    t.integer "quest_id"
+    t.integer "question_id"
+    t.integer "value"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "companies", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

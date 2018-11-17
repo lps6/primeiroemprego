0969f5c066c1361b7e4a0ad34d7043e149d85631
lps6 <lps6@cin.ufpe.br>
2018-11-17 13:07:09 -0300

    CUCUMBER RODANDO PORRAgit add .git add . TUDE VERDE NESSE CARAIgit add .!

--- features/registrations_controller.rb
+++ features/registrations_controller.rb
@@ -18,38 +18,74 @@ Then(/^I should be on the page ([^"]*)/) do |arg1|
   when "Sign Up"
     expect(page).to current_path(new_user_registration_path)   
   end
+  case arg1
+  when "Dashboard"
+    expect(page).to current_path(dashboard_path)  
+  end
 end
 
-When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
+When("I fill in {string} with {string}") do |param, filler|
   case param
   when "Nome"
     page.find(param).fill_in(param, with: filler)
   end
-  when "CPF"
-    page.find(param).fill_in(param, with: filler)
-  end
-  when "Sobre Mim"
-    page.find(param).fill_in(param, with: filler)
-  end
-  when "Email"
-    page.find(param).fill_in(param, with: filler)
-  end
-  when "Senha"
-    page.find(param).fill_in(param, with: filler)
-  end
-  when "Confirmar Senha"
-    page.find(param).fill_in(param, with: filler)
-  end  
 end
 
-When(/^I select a valid birthdate$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
+  case param
+  when "user_cpf"
+    page.find(param).fill_in(param, with: filler)
+  end
 end
 
-When(/^I press "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
+  case param
+  when "user_birth_date"
+    page.find(param).fill_in(param, with: filler)
+  end
 end
 
-Then(/^I should be on the "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
+  case param
+  when "user_about_me"
+    page.find(param).fill_in(param, with: filler)
+  end
 end
+
+When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
+  case param
+  when "user_email"
+    page.find(param).fill_in(param, with: filler)
+  end
+end
+
+
+
+When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
+  case param
+  when "user_password"
+    page.find(param).fill_in(param, with: filler)
+  end
+end
+
+
+When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
+  case param
+  when "user_password_confirmation"
+    page.find(param).fill_in(param, with: filler)
+  end
+end
+
+When(/^I press ([^"]*)/) do |button|
+  case button
+  when 'Sign Up'
+    page.find('Sign Up').click
+  end
+end
+
+#Then(/^I should be on the ([^"]*)/) do |arg1|
+#  case arg1
+#  when "Dashboard"
+#    expect(page).to current_path(dashboard_path)   
+#  end
+#end

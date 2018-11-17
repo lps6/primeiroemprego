e297f827725d9b16c21331dc2e506cdd33257d6b
Himynameishenrique <hmf@cin.ufpe.br>
2018-11-17 16:51:11 -0300

    scenario da company

--- features/registrations_controller.rb
+++ features/registrations_controller.rb
@@ -18,74 +18,38 @@ Then(/^I should be on the page ([^"]*)/) do |arg1|
   when "Sign Up"
     expect(page).to current_path(new_user_registration_path)   
   end
-  case arg1
-  when "Dashboard"
-    expect(page).to current_path(dashboard_path)  
-  end
 end
 
-When("I fill in {string} with {string}") do |param, filler|
+When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
   case param
   when "Nome"
     page.find(param).fill_in(param, with: filler)
   end
-end
-
-When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
-  case param
-  when "user_cpf"
+  when "CPF"
     page.find(param).fill_in(param, with: filler)
   end
-end
-
-When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
-  case param
-  when "user_birth_date"
+  when "Sobre Mim"
     page.find(param).fill_in(param, with: filler)
   end
-end
-
-When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
-  case param
-  when "user_about_me"
+  when "Email"
     page.find(param).fill_in(param, with: filler)
   end
-end
-
-When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
-  case param
-  when "user_email"
+  when "Senha"
     page.find(param).fill_in(param, with: filler)
   end
-end
-
-
-
-When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
-  case param
-  when "user_password"
+  when "Confirmar Senha"
     page.find(param).fill_in(param, with: filler)
-  end
+  end  
 end
 
-
-When(/^I fill in ([^"]*) with ([^"]*)/) do |param, filler|
-  case param
-  when "user_password_confirmation"
-    page.find(param).fill_in(param, with: filler)
-  end
+When(/^I select a valid birthdate$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I press ([^"]*)/) do |button|
-  case button
-  when 'Sign Up'
-    page.find('Sign Up').click
-  end
+When(/^I press "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
-#Then(/^I should be on the ([^"]*)/) do |arg1|
-#  case arg1
-#  when "Dashboard"
-#    expect(page).to current_path(dashboard_path)   
-#  end
-#end
+Then(/^I should be on the "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end

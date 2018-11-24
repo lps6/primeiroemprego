0b5041c565eb32e45c8753b8acb5ce73449fdc82
gsf4 <gsf4@cin.ufpe.br>
2018-11-24 15:53:06 -0300

    Devise funcional em pt-BR

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,14 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.i18n.default_locale = :'pt-BR'
+
 
 module Primeiroemprego
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.i18n.default_locale = :'pt-BR'
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

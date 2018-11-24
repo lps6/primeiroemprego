ea2216ca435839acee77885ea0c9f8a89f1ca00f
gsf4 <gsf4@cin.ufpe.br>
2018-11-24 12:08:09 -0300

    Recursos do devise atualizados para pt-BR

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.i18n.default_locale = :'pt-BR'
 
 module Primeiroemprego
   class Application < Rails::Application

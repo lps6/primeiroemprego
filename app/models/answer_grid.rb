131349dd177393d106e1f7b09b4cb6348062bc28
gsf4 <gsf4@cin.ufpe.br>
2018-11-25 11:46:45 -0300

    Associação entre answers e users

--- app/models/answer_grid.rb
+++ app/models/answer_grid.rb
@@ -1,3 +1,4 @@
 class AnswerGrid < ApplicationRecord
-  belongs_to :question_grid
+  belongs_to :question_grid, foreign_key: 'question_grid_id'
+  belongs_to :user, foreign_key: 'user_id'
 end
--- db/migrate/20181124145258_create_answer_grids.rb
+++ db/migrate/20181124145258_create_answer_grids.rb
@@ -3,6 +3,9 @@ class CreateAnswerGrids < ActiveRecord::Migration[5.2]
     create_table :answer_grids do |t|
       t.integer :value
       t.belongs_to :question_grid, foreign_key: true
+      t.integer :user_id
+      t.integer :question_grid_id
+  
 
       t.timestamps
     end

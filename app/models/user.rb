8ccae5a5ae9b119aaf4c0ebe87a7ec43f087be1a
gsf4 <gsf4@cin.ufpe.br>
2018-11-25 20:49:14 -0300

    Corrigido bugs de edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,29 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
+
+
+def update_with_password(params, *options)
+current_password = params.delete(:current_password)
+
+if params[:password].blank?
+  params.delete(:password)
+  params.delete(:password_confirmation) if params[:password_confirmation].blank?
+end
+
+result = if params[:password].blank? || valid_password?(current_password)
+  update_attributes(params, *options)
+else
+  self.assign_attributes(params, *options)
+  self.valid?
+  self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+  false
+end
+
+clean_up_passwords
+result
+end
+
   def self.connect_to_linkedin(auth, signed_in_resource=nil) 
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if user
--- spec/features/users/user_edit_spec.rb
+++ spec/features/users/user_edit_spec.rb
@@ -12,8 +12,7 @@ feature 'User edit', :devise do
     login_as(user, :scope => :user)
     visit edit_user_registration_path(user)
     fill_in 'Email', :with => 'newemail@example.com'
-    fill_in 'Current password', :with => user.password
-    click_button 'Update'
+    click_button 'Atualizar'
     txts = [I18n.t( 'devise.registrations.updated'), I18n.t( 'devise.registrations.update_needs_confirmation')]
     expect(page).to have_content(/.*#{txts[0]}.*|.*#{txts[1]}.*/)
   end

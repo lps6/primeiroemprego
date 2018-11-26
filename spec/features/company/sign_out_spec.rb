55f8a6546c37ae11522d4d046d7cf54ef534f66a
Ewerton <effs@cin.ufpe.br>
2018-11-26 11:16:31 -0300

    Teste signout company correto

--- spec/features/company/sign_out_spec.rb
+++ spec/features/company/sign_out_spec.rb
@@ -1,9 +1,9 @@
-require "spec_helper"
+
 feature 'Sign out', :devise do
 
     scenario 'company signs out successfully' do
       company = FactoryGirl.create(:company)
-      signin(company.email, company.password)
+      sign_in_company(company.email, company.password)
       expect(page).to have_content I18n.t 'devise.sessions.signed_in'
       click_link 'Logout'
       expect(page).to have_content I18n.t 'devise.sessions.signed_out'

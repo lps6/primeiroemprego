ef0bb52d50ab6ca584d5b7ac037a664a2eacbfcd
gsf4 <gsf4@cin.ufpe.br>
2018-11-26 18:54:12 -0300

    spec de deletar quest

--- spec/features/quests/company_sees_quest_spec.rb
+++ spec/features/quests/company_sees_quest_spec.rb
@@ -1,5 +1,5 @@
-feature 'Sign in', :devise do 
-    scenario 'company can sign in with valid credentials' do
+feature 'Comapany can see quests', :devise do 
+    scenario 'company can see quests on dashboard' do
       company = FactoryGirl.create(:company)
       sign_in_company(company.email, company.password)
       create(:quest, company: company)
@@ -7,4 +7,13 @@ feature 'Sign in', :devise do
       expect(page).to have_content "This is a test for a quest"
     end
 
+    scenario 'company can view quest from dashboard' do
+        company = FactoryGirl.create(:company)
+        sign_in_company(company.email, company.password)
+        quest =create(:quest, company: company)
+        visit root_path(company)
+        find('#btn-show-quest').click
+        expect(page).to have_current_path (quest_path(quest.id))
+        expect(page).to have_content "This is a test for a quest"
+      end
   end

82399565f56dea2b2daf3fd275ac9084419ac525
gsf4 <gsf4@cin.ufpe.br>
2018-11-25 22:40:49 -0300

    Adicionado teste de cancelamento de cadastro

--- spec/features/users/user_delete_spec.rb
+++ spec/features/users/user_delete_spec.rb
@@ -2,19 +2,19 @@ require "spec_helper"
 include Warden::Test::Helpers
 Warden.test_mode!
 
-feature 'User delete', :devise, :js do
+
+feature 'User profile page', :devise do
 
   after(:each) do
     Warden.test_reset!
   end
 
-  scenario 'user can delete own account' do
-    skip 'skip a slow test'
+
+  scenario 'user sees dashboard' do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
     visit edit_user_registration_path(user)
-    click_button 'Cancel my account'
-    page.driver.browser.switch_to.alert.accept
+    click_button 'Cancelar meu cadastro'
     expect(page).to have_content I18n.t 'devise.registrations.destroyed'
   end
 
